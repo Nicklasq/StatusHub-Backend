d9578d0e5765d9c351a1fb527b506b39ac07eee8
nicklasq <nicklascao@MacBookPro.utopia.net>
2025-11-17 16:30:28 +0100

    Start

---
+++ src/main/java/com/statushub/statushub/StatushubApplication.java
@@ -0,0 +1,13 @@
+package com.statushub.statushub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatushubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatushubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/statushub/statushub/StatushubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statushub.statushub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatushubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
